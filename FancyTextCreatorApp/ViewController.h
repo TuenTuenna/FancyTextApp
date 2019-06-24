76eee0ebbefdca018567bfc86a63c00da1453a5c
kor216465 <kor216465@gmail.com>
2019-06-25 03:50:14 +0900

    first commit

--- FancyTextCreatorApp/ViewController.h
+++ FancyTextCreatorApp/ViewController.h
@@ -8,7 +8,39 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+    
+    BOOL state;
+    
+    
+}
+
+
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+
+
+@property (weak, nonatomic) IBOutlet UIButton *addShadowButton;
+
+
+// 엔터를 쳤을때 나가도록 하기 위해 did on exit 으로 설정해서 action 을 넣어준다.
+- (IBAction)dismissKeyBoard:(id)sender;
+
+- (IBAction)makeRed:(id)sender;
+- (IBAction)makeBlue:(id)sender;
+- (IBAction)makeGreen:(id)sender;
+
+- (IBAction)font1:(id)sender;
+- (IBAction)font2:(id)sender;
+- (IBAction)font3:(id)sender;
+- (IBAction)font4:(id)sender;
+
+- (IBAction)addShadow:(id)sender;
+- (IBAction)small:(id)sender;
+- (IBAction)medium:(id)sender;
+- (IBAction)large:(id)sender;
+
 
 
 @end
--- FancyTextCreatorApp/ViewController.m
+++ FancyTextCreatorApp/ViewController.m
@@ -20,4 +20,92 @@
 }
 
 
+- (IBAction)dismissKeyBoard:(id)sender {
+    // 텍스트 입력창에 적힘 글을 라벨에 띄운다.
+    self.label.text = self.textField.text;
+    [self resignFirstResponder];
+    
+}
+
+- (IBAction)makeRed:(id)sender {
+    
+    UIColor *red = [UIColor redColor];
+    [self.label setTextColor:red];
+    
+}
+
+- (IBAction)makeBlue:(id)sender {
+    
+    UIColor *blue = [UIColor blueColor];
+    [self.label setTextColor:blue];
+    
+}
+
+- (IBAction)makeGreen:(id)sender {
+    
+    UIColor *green = [UIColor greenColor];
+    [self.label setTextColor:green];
+    
+}
+
+- (IBAction)font1:(id)sender {
+    [self.label setFont:[UIFont fontWithName:@"Blacksword" size:24]];
+}
+
+- (IBAction)font2:(id)sender {
+     [self.label setFont:[UIFont fontWithName:@"LemonMilk" size:24]];
+}
+
+- (IBAction)font3:(id)sender {
+    [self.label setFont:[UIFont fontWithName:@"Moon Flower" size:24]];
+}
+
+- (IBAction)font4:(id)sender {
+    [self.label setFont:[UIFont fontWithName:@"SugarstyleMillenial-Regular" size:24]];
+}
+
+- (IBAction)addShadow:(id)sender {
+    
+    if(state == true){
+        // 쉐도우 뺴기
+        self.label.layer.shadowOpacity = 0;
+        state = false;
+        // 쉐도우 버튼 글자
+        [self.addShadowButton setTitle:@"Add Shadow" forState:UIControlStateNormal];
+        
+    } else {
+        // 쉐도우 넣기
+        self.label.layer.shadowColor = [[UIColor blackColor] CGColor];
+        self.label.layer.shadowOpacity = 0.25;
+        self.label.layer.shadowRadius = 2.0f;
+        self.label.layer.shadowOffset = CGSizeMake(2, 2);
+        state = true;
+        
+        [self.addShadowButton setTitle:@"Remove Shadow" forState:UIControlStateNormal];
+        
+    }
+    
+   
+    
+}
+
+- (IBAction)small:(id)sender {
+    UIFont *font = self.label.font;
+    self.label.font = [font fontWithSize:10];
+}
+
+- (IBAction)medium:(id)sender {
+    UIFont *font = self.label.font;
+    self.label.font = [font fontWithSize:25];
+}
+
+- (IBAction)large:(id)sender {
+    UIFont *font = self.label.font;
+    self.label.font = [font fontWithSize:45];
+}
+
+
+
+
+
 @end
